d71b770a10037c48b9c9fcd498316488e2c24919
PR7175Z <aadimpraz@gmail.com>
2024-08-29 20:35:33 +0545

    Removed comments

--- diabetesprediction/apicaller.py
+++ diabetesprediction/apicaller.py
@@ -6,11 +6,4 @@ url = 'http://127.0.0.1:8000/predict'
 data = {'features': [59, 2,32.1, 101,157, 93.2, 38, 4, 4.8598,87 ]}
 response = requests.post(url, json=data)
 
-# if(response.json()['prediction'] == '0'):
-#     print('sedusa')
-# elif(response.json()['prediction'] == '1'):
-#     print('versicolor')
-# else:
-#     print('Virginca')
-
 print(response.json())
