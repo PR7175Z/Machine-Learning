fabfea09f8d9c9acda02ac3807e053a269616f45
PR7175Z <aadimpraz@gmail.com>
2024-09-01 06:38:56 +0545

    Read response from API

--- mushroomclassifier/apicaller.py
+++ mushroomclassifier/apicaller.py
@@ -2,4 +2,8 @@ import requests
 
 api = 'http://127.0.0.1:8000/predict'
 
-data = {'features': []}
+data = {'features': ['b', 'y', 'w', 't', 'a', 'f', 'c', 'b', 'g', 'e', 'c', 's', 's', 'w','w', 'w','p','w','o', 'p', 'k', 's', 'g']}
+
+response = requests.json(api, json=data)
+
+print(response)
