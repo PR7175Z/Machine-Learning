3a8c13a606a8e85c623c4a6f2d12abe5ba3d9c56
PR7175Z <aadimpraz@gmail.com>
2024-09-01 22:09:50 +0545

    Added response

--- mushroomclassifier/apicaller.py
+++ mushroomclassifier/apicaller.py
@@ -4,6 +4,6 @@ api = 'http://127.0.0.1:8000/predict'
 
 data = {'features': ['b', 'y', 'w', 't', 'a', 'f', 'c', 'b', 'g', 'e', 'c', 's', 's', 'w','w', 'w','p','w','o', 'p', 'k', 's', 'g']}
 
-response = requests.json(api, json=data)
+response = requests.post(api, json=data)
 
-print(response)
+print(response.json())
