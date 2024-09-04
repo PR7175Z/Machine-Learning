13430b74648b702cad38d318dbd3506d8cc3bc72
PR7175Z <aadimpraz@gmail.com>
2024-09-04 18:40:33 +0545

    Changed return value to int

--- diamondpriceprediction/diamondprediction.py
+++ diamondpriceprediction/diamondprediction.py
@@ -15,6 +15,6 @@ def predict(data:feature):
     try:
         features = np.array(data.features).reshape(1, -1)
         prediction = loaded_model.predict(features)
-        return prediction
+        return int(prediction)
     except Exception as e:
         return HTTPException(status_code = 500, detail=str(e) )
