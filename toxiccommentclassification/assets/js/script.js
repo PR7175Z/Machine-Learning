00d68e9e3ad168298ff6661100517daee86e2e19
PR7175Z <aadimpraz@gmail.com>
2024-09-30 22:58:46 +0545

    API calling fixes

--- toxiccommentclassification/assets/js/script.js
+++ toxiccommentclassification/assets/js/script.js
@@ -4,24 +4,30 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     async function getResponse(comment) {
         const api = 'http://127.0.0.1:8080/predict';
+
+        const req = {
+            'comment' : comment
+        }
+
         const response = await fetch(api,{
                 method : 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body:JSON.stringify({'comment':comment})
+                body:JSON.stringify(req)
             }
         )
 
         if (!response.ok) {
+            const errorData = await response.json(); // Get error details
+            console.log("Error response:", errorData); // Log error message
             throw new Error(`HTTP error! status: ${response.status}`);
         }
+
         const data = await response.json();
         console.log(data);
         return data;
     }
-
-    console.log(JSON.stringify({'comment':comment}));
 
     getResponse(comment);
 })
