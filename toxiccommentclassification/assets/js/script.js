24896ce5a765bfff14297c77b783e82d4db055b6
PR7175Z <aadimpraz@gmail.com>
2024-09-30 14:14:43 +0545

    Function calling

--- toxiccommentclassification/assets/js/script.js
+++ toxiccommentclassification/assets/js/script.js
@@ -1,19 +1,27 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    async function getResponse(comment) {
-        api = 'http://127.0.0.1:8080/predict';
+    comment = "It was a bad movie";
+    // comment = "It was a great movie";
 
+    async function getResponse(comment) {
+        const api = 'http://127.0.0.1:8080/predict';
         const response = await fetch(api,{
-            method : 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body:JSON.stringify(comment)
-        })
+                method : 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify({'comment':comment})
+            }
+        )
 
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
+        console.log(data);
         return data;
     }
+
+    console.log(JSON.stringify({'comment':comment}));
+
+    getResponse(comment);
 })
