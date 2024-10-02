b0870576475460f87431eecb7a93013d1254b312
PR7175Z <aadimpraz@gmail.com>
2024-10-02 08:14:06 +0545

    Event listener for form submission

--- toxiccommentclassification/assets/js/script.js
+++ toxiccommentclassification/assets/js/script.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    // comment = "It was a bad movie";
-    comment = "It was a great movie";
 
     async function getResponse(comment) {
         const api = 'http://127.0.0.1:8080/predict';
@@ -25,9 +23,22 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
 
         const data = await response.json();
-        console.log(data);
         return data;
     }
 
-    getResponse(comment);
+    document.getElementById('commentform').addEventListener('submit', async (e)=>{
+        e.preventDefault();
+        const cmt = document.querySelector('#commentform textarea').value;
+        const resultdisplay = document.getElementById('result');
+        const res = await getResponse(cmt);
+
+        let resulttext = '';
+
+        if(res['pred'] === 0){
+            resulttext = 'Non Toxic';
+        }else{
+            resulttext = 'Toxic';
+        }
+        resultdisplay.innerHTML = `This comment is ${resulttext}`;
+    });
 })
